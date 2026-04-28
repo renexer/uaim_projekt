be3d699d5337dea953b7f2fced1b94cbd919376f
Mati Pacek <levelsen@wp.pl>
2026-04-28 17:06:33 +0200

    Wspólne utils, errors etc

---
+++ backend/app/utils/auth.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+
+from app.models import User
+from app.utils.errors import ForbiddenError, UnauthorizedError
+
+
+def get_current_user() -> User:
+    identity = get_jwt_identity()
+    if not identity:
+        raise UnauthorizedError("Brak tożsamości użytkownika.")
+    user = User.query.get(identity)
+    if not user:
+        raise UnauthorizedError("Użytkownik nie istnieje.")
+    return user
+
+
+def roles_required(*role_names: str):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            user = get_current_user()
+            user_role_names = {role.name for role in user.roles}
+            if not user_role_names.intersection(set(role_names)):
+                raise ForbiddenError("Brak wymaganych uprawnień.")
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
